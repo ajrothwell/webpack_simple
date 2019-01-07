1f8055bab814e435cbdef6e45c648e598d352c01
andy.rothwell <rothwell.andy@gmail.com>
2019-01-07 15:04:45 -0500

    fixes site

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import { square } from './fun';
+import { square } from './localFunctions.js';
 import { multiply } from 'rollup_simple_3';
 import { double } from 'rollup_simple_3';
 import { barfunc } from 'rollup_simple_3';
 
-// console.log('multiply:', multiply);
-
-document.getElementById('test').innerHTML = square(10);
-document.getElementById('test2').innerHTML = multiply(12);
-document.getElementById('test3').innerHTML = barfunc();
-document.getElementById('test4').innerHTML = double(5);
+document.getElementById('square-value').innerHTML = square(10);
+document.getElementById('multiply-value').innerHTML = multiply(12);
+document.getElementById('barfunc-value').innerHTML = barfunc();
+document.getElementById('double-value').innerHTML = double(5);
