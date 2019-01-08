94744b33204037cbe105ad2271118d2b3e1eb7a3
andy.rothwell <rothwell.andy@gmail.com>
2019-01-08 08:44:35 -0500

    fixes bug in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { square } from './localFunctions.js';
-import { multiply } from 'rollup_simple_3';
-import { double } from 'rollup_simple_3';
-import { barfunc } from 'rollup_simple_3';
+import { multiply } from 'rollup_simple_1';
+import { double } from 'rollup_simple_1';
+import { barfunc } from 'rollup_simple_1';
 
 document.getElementById('square-value').innerHTML = square(10);
 document.getElementById('multiply-value').innerHTML = multiply(12);
