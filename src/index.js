2a64757c39f79ae4d83cf015cb279c557b30b83a
andy.rothwell <rothwell.andy@gmail.com>
2019-01-08 10:05:18 -0500

    new import of rollup_simple_1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { square } from './localFunctions.js';
-import { multiply } from 'rollup_simple_1';
 import { double } from 'rollup_simple_1';
+import { triple } from 'rollup_simple_1';
 import { barfunc } from 'rollup_simple_1';
 
 document.getElementById('square-value').innerHTML = square(10);
-document.getElementById('multiply-value').innerHTML = multiply(12);
+document.getElementById('double-value').innerHTML = double(10);
 document.getElementById('barfunc-value').innerHTML = barfunc();
-document.getElementById('double-value').innerHTML = double(5);
+document.getElementById('triple-value').innerHTML = triple(10);
